875637e11c65fe4f032d9b29c68e3587306dead2
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-16 16:06:57 +0530

    Added: Scenario to check if the position 0 0 in TicTacToe game board is initialized to null character ('\0')

--- src/main/java/com/bnppf/kata/TicTacToeGame.java
+++ src/main/java/com/bnppf/kata/TicTacToeGame.java
@@ -1,4 +1,9 @@
 package com.bnppf.kata;
 
 public class TicTacToeGame {
+    private char[][] ticTacToeGameBoard = new char[3][3];
+
+    public char getTicTacToeGameBoard(int row, int column) {
+        return ticTacToeGameBoard[row][column];
+    }
 }
--- src/test/java/com/bnppf/kata/TicTacToeGameTest.java
+++ src/test/java/com/bnppf/kata/TicTacToeGameTest.java
@@ -2,6 +2,7 @@ package com.bnppf.kata;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class TicTacToeGameTest {
@@ -13,4 +14,10 @@ public class TicTacToeGameTest {
 
     }
 
+    @Test
+    public void checkIfTicTacToeGameBoardPositionZeroZeroIsInitializedToNullCharacter() {
+
+        assertEquals('\0', new TicTacToeGame().getTicTacToeGameBoard(0, 0));
+
+    }
 }
