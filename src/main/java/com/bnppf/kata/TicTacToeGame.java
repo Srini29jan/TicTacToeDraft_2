f7a34163f31fe723d8f62e0804bc983b35e4638c
Srinivas Subburaj <ssrinivas29jan@gmail.com>
2020-08-16 16:52:55 +0530

    Refactored: Moved the initialization part of ticTacToeGameBoard to constructor

--- src/main/java/com/bnppf/kata/TicTacToeGame.java
+++ src/main/java/com/bnppf/kata/TicTacToeGame.java
@@ -2,8 +2,11 @@ package com.bnppf.kata;
 
 public class TicTacToeGame {
     private static final int NUMBER_OF_GRIDS_IN_GAME_BOARD = 3;
-    private char[][] ticTacToeGameBoard = new char[NUMBER_OF_GRIDS_IN_GAME_BOARD][NUMBER_OF_GRIDS_IN_GAME_BOARD];
+    private char[][] ticTacToeGameBoard;
 
+    public TicTacToeGame() {
+        ticTacToeGameBoard = new char[NUMBER_OF_GRIDS_IN_GAME_BOARD][NUMBER_OF_GRIDS_IN_GAME_BOARD];
+    }
     public char getTicTacToeGameBoard(int row, int column) {
         return ticTacToeGameBoard[row][column];
     }
